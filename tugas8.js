514c976ce2fe2f5ba11d15058f8ac972ea6fb962
Bayu-aditiya-saputra <bayurikasolihat@gmail.com>
2022-01-05 22:06:56 +0700

    tugas 8 js
    tugas untuk niomic

---
+++ tugas8.js
@@ -0,0 +1,13 @@
+function panggilobject() {
+    var buku = {
+        judul: "sikancil",
+        penerbit: "media graha",
+        tahun: "2009"
+    }
+    
+    console.log(buku)
+    buku.penerbit = "Graha media"
+    console.log(buku)
+}
+
+panggilobject();
